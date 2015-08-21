41db524202e55c9a76ddba366c2e882f69aff643
Ryan <ryan.samarajeewa@gmail.com>
2015-08-21 11:17:00 -0700

    Add material design skin

--- Desktop Flashcards/Form1.cs
+++ Desktop Flashcards/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace Desktop_Flashcards
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
+        private readonly MaterialSkinManager materialSkinManager;
         public Form1()
         {
             InitializeComponent();
+
+            // Initialize MaterialSkinManager
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
     }
 }
